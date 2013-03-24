c203bb06d66c22b5ffed8c9ca804302c6cef72d7
Adrian Cheung <adrianmcheung@gmail.com>
2013-03-24 15:30:14 -0400

    added different shape types to shape class and parameters for point class

--- js/main.js
+++ js/main.js
@@ -77,9 +77,54 @@ function draw() {
 	}
 }
 
-function Shape() {
+function Shape(type) {
 	this.points = [];
 	this.moving = true;
+
+	switch(type){
+		case 'O':
+			this.points.push(new Point(0,0));
+			this.points.push(new Point(0,1));
+			this.points.push(new Point(1,0));
+			this.points.push(new Point(1,1));
+			break;
+		case 'L':
+			this.points.push(new Point(0,0));
+			this.points.push(new Point(0,1));
+			this.points.push(new Point(0,2));
+			this.points.push(new Point(1,2));
+			break;
+		case 'J':
+			this.points.push(new Point(1,0));
+			this.points.push(new Point(1,1));
+			this.points.push(new Point(1,2));
+			this.points.push(new Point(0,2));
+			break;
+		case 'S':
+			this.points.push(new Point(1,0));
+			this.points.push(new Point(2,0));
+			this.points.push(new Point(0,1));
+			this.points.push(new Point(1,1));
+			break;
+		case 'Z':
+			this.points.push(new Point(0,0));
+			this.points.push(new Point(0,1));
+			this.points.push(new Point(1,1));
+			this.points.push(new Point(1,2));
+			break;
+		case 'I':
+			this.points.push(new Point(0,0));
+			this.points.push(new Point(0,1));
+			this.points.push(new Point(0,2));
+			this.points.push(new Point(0,3));
+			break;
+		case 'T':
+			this.points.push(new Point(0,0));
+			this.points.push(new Point(1,0));
+			this.points.push(new Point(2,0));
+			this.points.push(new Point(1,1));
+			break;
+	}
 }
 
 Shape.prototype.draw = function() {
@@ -88,9 +133,9 @@ Shape.prototype.draw = function() {
 	}
 };
 
-function Point() {
-	this.x = 0;
-	this.y = 0;
+function Point(x,y) {
+	this.x = x;
+	this.y = y;
 	this.w = 40;
 	this.h = 40;
 	this.fill = '#000FF';
