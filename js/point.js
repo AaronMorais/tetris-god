82e3a7bdb56ba5dbc76330392a8f33ea3319ca21
Aaron Morais <aaron.m@live.ca>
2013-03-24 19:01:58 -0400

    Merge branch 'master' of github.com:AaronMorais/tetris-god
    Conflicts:
    	js/main.js
    	js/shape.js

--- js/point.js
+++ js/point.js
@@ -1,16 +1,17 @@
 function Point(x,y) {
+	var blocksize = 30;
 	this.x = x;
 	this.y = y;
-	this.w = 40;
-	this.h = 40;
+	this.w = blocksize;
+	this.h = blocksize;
 	this.fill = '#000FF';
 
 	this.draw = function() {
 		ctx.fillStyle = this.fill;
-		ctx.fillRect(this.x*40, this.y*40, this.w, this.h);
-	}
+		ctx.fillRect(this.x*blocksize, this.y*blocksize, this.w, this.h);
+	};
 
 	this.gravity = function() {
 		this.y +=1;
-	}
+	};
 }
