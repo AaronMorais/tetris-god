12d5d757a0b82bc1840646502385074c5c560541
Aaron Morais <aaron.m@live.ca>
2013-03-24 21:49:51 -0400

    Start working on god mode with socket.io and god.html. Fix for line clearing issue on more than one line simultaneously.

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 var connect = require('connect');
-connect.createServer(
-        connect.static(__dirname)
-).listen(8080);
+var io = require('socket.io')
+
+var server = connect.createServer(
+	connect.static(__dirname)
+).listen(8080)
+socket = io.listen(server);
+
+socket.on('connection', function (socket) {
+    socket.on('setNextType', function (data) {
+    	console.log(data);
+        socket.broadcast.emit("nextType",data);
+    });
+});
